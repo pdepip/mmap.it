12e1d9fd61ec000e90f955fc460f19db46ca1c0a
Patrick DePippo <pdepip@gmail.com>
2019-10-25 11:56:50 -0400

    first working commit

---
+++ src/renderer/components/Application.tsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import * as React from 'react';
+
+const Application = () => (
+    <div>
+        Hello world
+    </div>
+);
+
+export default hot(Application);
